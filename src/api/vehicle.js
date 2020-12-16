182fe909f96a4d2428fea1df0b1e2de6ef53f50f
Pongpeera Kundilokborirak <pongpeera.kun@gmail.com>
2020-12-16 10:33:03 +0900

    removed unused views

--- src/api/vehicle.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchStatus(query) {
-  return request({
-    // url: `/users?perPage=${query.limit}&page=${query.page}`,
-    url: '/vue-element-admin/vehicle/satus',
-    method: 'get'
-  })
-}
